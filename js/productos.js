d9076a72eb8cf7f55385378577b2818ceb71a840
diegun99 <diegun123456789@gmail.com>
2021-10-05 23:45:23 -0500

    catalogo basico con json

--- js/productos.js
+++ js/productos.js
@@ -1,39 +1,83 @@
-const imagenes = [
-    {
-        src: 'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/5937e90a5bafe882f5bc09e6/gatitos-cesta_0.jpg',
-        alt: 'Gatitos, no se puede decir más',
-        nombre: 'Artículo 1',
-        precio: 52
-    },
-    {
-        src: 'https://www.hola.com/imagenes/estar-bien/20180925130054/consejos-para-cuidar-a-un-gatito-recien-nacido-cs/0-601-526/cuidardgatito-t.jpg',
-        alt: 'Gatitos, no se puede decir más',
-        nombre: 'Artículo 2',
-        precio: 82
-    },
-    {
-        src: 'https://www.zooplus.es/magazine/wp-content/uploads/2018/04/fotolia_169457098.jpg',
-        alt: 'Gatitos, no se puede decir más',
-        nombre: 'Artículo 3',
-        precio: 99
-    },
-];
 
-function renderizarGaleria(imagenes) {
-    let html = '';
 
-    imagenes.forEach(function (imagen) {
-        html += `
-        <div class="galeria-item">
-          <img src="${imagen.src}" alt="${imagen.alt}" width="200px" />
-          <h3>${imagen.nombre}</h3>
-          <p>${imagen.precio}€</p>
-        </div>
-      `;
-    });
-    $('#galeria').html(html);
-}
+$(document).ready(function() {
+    const imagenes = [
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        },
+        {
+            src: "https://cdn1.totalcode.net/easy/product-zoom/es/tornillo-lamina-8-mm-x-3~4%22-avellanado-phillips-zincado-x12und-3.webp",
+            alt: 'Gatitos, no se puede decir más',
+            titulo: 'COPA STANLEY',
+            precio: "HEX. M1/2  30mm",
+            descripcion: "",
+            
+        }
+    ];
 
-$(function () {
-    renderizarGaleria(imagenes);
-});
+    var j = 0;
+    var content = "";
+    for (var i in imagenes) {
+      if (i % 3 === 0) {
+        $("#productos").append(`<div id="row${i}" class="row"></div>`);
+        j = i;
+      }
+      $("#row" + j).append(`
+      <div class="col-md-4">
+      <section class="caja-catalogo">
+      <img
+        src="${imagenes[i].src}"
+        width=60% alt="${imagenes[i].alt}">
+      <hr width="100%" size="3" noshade="noshade">
+      <h2> ${imagenes[i].titulo}</h2>
+      <p>HEX. M1/2  30mm</p>
+      <button class="boton-catalogo">Ver más</button>
+    </section>
+    </div>`);
+    }
+  });
